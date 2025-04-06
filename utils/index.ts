c73b03c5dcfd64ebd27a5b0a84bb48658369fbe6
baoguosen <2563209120@qq.com>
2025-04-06 15:59:07 +0800

    chore:  游戏界面 player.me 始终在右边第一个

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,25 @@
 import type { Player } from '@/types'
 
-
+/**
+ * 将数组分成两半，player.me 在右边第一个
+ * @param arr 数组
+ * @returns 两半的数组
+ */
 function splitArray(arr: Player[]) {
-    const mid = Math.floor(arr?.length / 2);
-    const firstHalf = arr?.slice(0, mid);
-    const secondHalf = arr?.slice(mid);
+	const mid = Math.floor(arr?.length / 2);
+	const firstHalf = arr?.slice(0, mid);
+	const secondHalf = arr?.slice(mid);
 
-    return [firstHalf, secondHalf];
+	const leftPlayers = firstHalf.filter(player => !player.me);
+	const rightPlayers = secondHalf.filter(player => !player.me);
+
+	const me = secondHalf.find(player => player.me) || firstHalf.find(player => player.me);
+
+	if (me) {
+		rightPlayers.unshift(me);
+	}
+
+	return [leftPlayers, rightPlayers];
 }
 
 export { splitArray };
